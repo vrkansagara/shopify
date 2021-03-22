d26d9896c00c81f9eeafa529daa6a134a2a823f3
Vallabh Kansagara <vrkansagara@gmail.com>
2021-03-22 21:09:23 +0530

    Adding delete API

--- src/Order.php
+++ src/Order.php
@@ -6,9 +6,24 @@ use GuzzleHttp\Psr7\Response;
 
 class Order extends Shopify
 {
+	public $resourceKey = 'orders';
+
     public function getAll(): Response
     {
-        $apiResource = 'orders.json';
+        $apiResource = sprintf('%s.json',$this->resourceKey);
         return $this->client->get($apiResource);
     }
+
+    public function get(int $id): Response
+    {
+        $apiResource = sprintf('%s/%d.json',$this->resourceKey,$id);
+        return $this->client->get($apiResource);
+	}
+
+    public function delete(int $id): Response
+    {
+        $apiResource = sprintf('%s/%d.json',$this->resourceKey,$id);
+        return $this->client->delete($apiResource);
+	}
+
 }
--- src/Product.php
+++ src/Product.php
@@ -6,9 +6,23 @@ use GuzzleHttp\Psr7\Response;
 
 class Product extends Shopify
 {
+	public $resourceKey = 'products';
+
     public function getAll(): Response
     {
-        $apiResource = 'products.json';
+        $apiResource = sprintf('%s.json',$this->resourceKey);
         return $this->client->get($apiResource);
     }
+
+    public function get(int $id): Response
+    {
+        $apiResource = sprintf('%s/%d.json',$this->resourceKey,$id);
+        return $this->client->get($apiResource);
+	}
+
+    public function delete(int $id): Response
+    {
+        $apiResource = sprintf('%s/%d.json',$this->resourceKey,$id);
+        return $this->client->delete($apiResource);
+	}
 }
