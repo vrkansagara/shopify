ef5c7542761023d17a06cc803b5d01b528191b77
Vallabh Kansagara <vrkansagara@gmail.com>
2021-04-03 10:47:17 +0530

    adding test annotation

--- test/ShopifyClientTest.php
+++ test/ShopifyClientTest.php
@@ -16,40 +16,37 @@ use function get_class;
 
 class ShopifyClientTest extends TestCase
 {
-    /** @var Shopify $shopify */
-    public $shopify;
+	/** @var Shopify $shopify */
+	public $shopify;
 
-    protected function setUp(): void
-    {
-        $this->shopify = new Shopify();
-    }
+	protected function setUp(): void
+	{
+		$this->shopify = new Shopify();
+	}
 
-    public function testClientMustBeAnObject()
-    {
-        $this->assertIsObject($this->shopify);
-    }
+	/** @test */
+	public function clientMustBeAnObject()
+	{
+		$this->assertIsObject($this->shopify);
+	}
 
-    public function testClientMustBeAnInstanceofGuzzleHttp()
-    {
-        $httpClient = new Client();
-        $this->assertInstanceOf(get_class($httpClient), $this->shopify->getClient());
-    }
+	/** @test */
+	public function clientMustBeAnInstanceofGuzzleHttp()
+	{
+		$httpClient = new Client();
+		$this->assertInstanceOf(get_class($httpClient), $this->shopify->getClient());
+	}
 
-    public function testClientConfiguratioMustBeAnArray()
-    {
-
-       //
-    }
-
-    public function testClientCanAcceptArrayAsConfiguration()
-    {
-        $configuratio = [
-            'allow_redirects' => true,
-            'exceptions'      => true,
-            'decode_content'  => true,
-        ];
-        $this->shopify->setConfig($configuratio);
-        $config = $this->shopify->getConfig();
-        $this->assertEquals($configuratio,$config);
-    }
+	/** @test */
+	public function clientCanAcceptArrayAsConfiguration()
+	{
+		$configuratio = [
+			'allow_redirects' => true,
+			'exceptions'      => true,
+			'decode_content'  => true,
+		];
+		$this->shopify->setConfig($configuratio);
+		$config = $this->shopify->getConfig();
+		$this->assertEquals($configuratio,$config);
+	}
 }
