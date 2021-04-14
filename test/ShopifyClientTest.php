573b44ddddf9824f306c024576158e666e182984
Vallabh Kansagara <vrkansagara@gmail.com>
2021-04-14 20:00:33 +0530

    random

--- test/ShopifyClientTest.php
+++ test/ShopifyClientTest.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 use Vrkansagara\Shopify\Shopify;
 
-use function Couchbase\defaultDecoder;
 use function get_class;
 
 /**
@@ -16,37 +15,37 @@ use function get_class;
 
 class ShopifyClientTest extends TestCase
 {
-	/** @var Shopify $shopify */
-	public $shopify;
+    /** @var Shopify $shopify */
+    public $shopify;
 
-	protected function setUp(): void
-	{
-		$this->shopify = new Shopify();
-	}
+    protected function setUp(): void
+    {
+        $this->shopify = new Shopify();
+    }
 
-	/** @test */
-	public function clientMustBeAnObject()
-	{
-		$this->assertIsObject($this->shopify);
-	}
+    /** @test */
+    public function clientMustBeAnObject()
+    {
+        $this->assertIsObject($this->shopify);
+    }
 
-	/** @test */
-	public function clientMustBeAnInstanceofGuzzleHttp()
-	{
-		$httpClient = new Client();
-		$this->assertInstanceOf(get_class($httpClient), $this->shopify->getClient());
-	}
+    /** @test */
+    public function clientMustBeAnInstanceofGuzzleHttp()
+    {
+        $httpClient = new Client();
+        $this->assertInstanceOf(get_class($httpClient), $this->shopify->getClient());
+    }
 
-	/** @test */
-	public function clientCanAcceptArrayAsConfiguration()
-	{
-		$configuratio = [
-			'allow_redirects' => true,
-			'exceptions'      => true,
-			'decode_content'  => true,
-		];
-		$this->shopify->setConfig($configuratio);
-		$config = $this->shopify->getConfig();
-		$this->assertEquals($configuratio,$config);
-	}
+    /** @test */
+    public function clientCanAcceptArrayAsConfiguration()
+    {
+        $configuratio = [
+            'allow_redirects' => true,
+            'exceptions'      => true,
+            'decode_content'  => true,
+        ];
+        $this->shopify->setConfig($configuratio);
+        $config = $this->shopify->getConfig();
+        $this->assertEquals($configuratio, $config);
+    }
 }
